82ee853d92049645046d89b70a508d2d1ef73b50
Yacine Hmito <yacine.hmito@gmail.com>
2019-02-24 19:23:43 +0100

    run on each commit

--- .circleci/run_on_each_commit.sh
+++ .circleci/run_on_each_commit.sh
@@ -7,14 +7,13 @@ set -e
 cd $(dirname "$0")/..
 
 if [ "$CIRCLE_BRANCH" = "master" ]; then
-  yarn install --frozen-lockfile
   eval "$1"
 else
   git fetch -u origin master
-  
+  git checkout $CIRCLE_BRANCH
   GIT_SEQUENCE_EDITOR=true GIT_COMMITTER_NAME='Circle CI' GIT_COMMITTER_EMAIL='dev@spendesk.com' \
     git rebase origin/master \
     --interactive \
     --autosquash \
-    --exec "yarn install --frozen-lockfile && eval '$1'"
+    --exec "eval '$1'"
 fi
