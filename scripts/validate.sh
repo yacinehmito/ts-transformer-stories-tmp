329f458aad9eec7b76b844a621bf179fec6e9c46
Yacine Hmito <yacine.hmito@gmail.com>
2019-02-24 14:46:10 +0100

    fix reporter

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,14 +1,18 @@
 #/usr/bin/env bash
 
+set -e
+
 if [ -z "$CI" ]; then
   yarn lint
   yarn typecheck
   yarn test --ci
   yarn build:api
 else
+  DIR=$(dirname "${BASH_SOURCE[0]}")
+  export JEST_JUNIT_OUTPUT="$DIR/../.temp/test-reports/junit/results.xml"
+
   yarn lint --quiet
   yarn typecheck
-  export JEST_JUNIT_OUTPUT=".temp/test-reports/junit/results.xml"
-  yarn test --ci --runInBand --coverage --reporters="jest-junit"
+  yarn test --ci --runInBand --coverage --reporters="$DIR/../node_modules/jest-junit"
   yarn build:api
 fi
