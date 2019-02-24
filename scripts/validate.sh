2fe3c729e032ffc4ae3f73206ba888d295c46059
Yacine Hmito <yacine.hmito@gmail.com>
2019-02-24 14:40:02 +0100

    store artifacts

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -8,7 +8,7 @@ if [ -z "$CI" ]; then
 else
   yarn lint --quiet
   yarn typecheck
-  export JEST_JUNIT_OUTPUT=".temp/test-reports/jest/results.xml"
+  export JEST_JUNIT_OUTPUT=".temp/test-reports/junit/results.xml"
   yarn test --ci --runInBand --coverage --reporters="jest-junit"
   yarn build:api
 fi
