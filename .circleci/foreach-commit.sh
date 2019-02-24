be73ad9270a3e8ca220301ee7db9bf60b2d8ab22
Yacine Hmito <yacine.hmito@gmail.com>
2019-02-24 13:30:03 +0100

    trying abstraction

---
+++ .circleci/foreach-commit.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Runs a command on each commit since master
+
+set -e
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+
+if [ "$CIRCLE_BRANCH" = "master" ]
+  eval "$1"
+else
+  git fetch -u origin master
+  
+  GIT_SEQUENCE_EDITOR=true GIT_COMMITTER_NAME='Circle CI' GIT_COMMITTER_EMAIL='dev@spendesk.com' \
+    git rebase origin/master
+    --interactive \
+    --autosquash \
+    --exec "yarn install --frozen-lockfile && eval '$1'"
+fi
